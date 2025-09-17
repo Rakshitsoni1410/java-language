b18076d3e614162305a428b2a28b05df39e42edf
rakshitsoni1410 <rakshitrsoni@gmail.com>
2025-09-17 15:44:15 +0530

    feref

--- r27.java
+++ r27.java
@@ -1,7 +1,24 @@
 import java.util.*;
 public class r27 {
-
+//warrper class 
     public static void main(String[] args) {
+       
+       //integer 
+        int val= 3324;
+        Integer clasInteger =val;
+        
+        System.out.println(val);
+        System.out.println(clasInteger);
+        //float 
+        float r =33.4f;
+        Float F =r;
+        System.out.println(r);
+        System.out.println(F);
+        //charchater 
+        String tm = "hi ";
+        String S = tm;
+        System.out.println(tm);
+        System.out.println(S);
         
     }
 }
