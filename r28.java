3b4ee86966e29cb9ab75d54057b48379e5bd3949
rakshitsoni1410 <rakshitrsoni@gmail.com>
2025-09-18 14:36:49 +0530

    Refactor formatting in add method for improved readability

--- r28.java
+++ r28.java
@@ -10,8 +10,9 @@ public class r28 {
     static void sum() {
         System.out.println(a + b);
     }
-    static void add(int a,int b ){
-        System.out.println(a+b);
+
+    static void add(int a, int b) {
+        System.out.println(a + b);
     }
 
     public static void main(String[] args) {
