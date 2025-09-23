00f9091b3cf919825fa5c4ed6b68047cab599788
= <pdikkkuu@gmail.com>
2025-09-23 14:45:39 +0530

    vfvvxcbfvb d

---
+++ r23.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Student {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+}
+
+public class r23 {
+    public static void main(String[] args) {
+
+        Student student = new Student("Rakshit", 20);
+
+        System.out.println("Name: " + student.getName());
+        System.out.println("Age: " + student.getAge());
+    }
+}
