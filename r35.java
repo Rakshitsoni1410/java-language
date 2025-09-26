3978133fb7caa3118459f24e5ded0dc2135b8a6b
= <pdikkkuu@gmail.com>
2025-09-26 14:42:01 +0530

    asdv g fb

---
+++ r35.java
@@ -0,0 +1,15 @@
+class Teacher {
+    void teach() {
+        System.out.println("teches to student ");
+    }
+}
+
+class student extends Teacher {
+    void stu(){
+        System.out.println(" study at class");
+    }
+}
+
+public class r35 {
+
+}
